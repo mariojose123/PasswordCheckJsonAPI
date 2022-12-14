b17165d6d7fcfbd21dbbb8b809cd4a7c87ce9c4c
Mariojose123 <mariofilho4k@gmail.com>
2022-12-14 19:08:27 -0300

    added more things to docker for example it output tests

--- STARTpw-check-RESTAPI.sh
+++ STARTpw-check-RESTAPI.sh
@@ -2,7 +2,7 @@ echo "BE AWARE SUDO IS REQUIRED"
 echo "This code will run docker script"
 echo "a error will happen if don't have docker"
 
-sudo docker build . -tag pwCheck-image
+sudo docker build . -t pwCheck-image
 sudo docker run -d -p 8080:8080 --name pwCheck-container pwCheck-imag
 mkdir testResults
 docker cp pwCheck-container:/app/PasswordCheckJsonAPI/cover.html /testResults
