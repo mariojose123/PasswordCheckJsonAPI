1153184822f1fd01683047861bcedb650467c63b
Mariojose123 <mariofilho4k@gmail.com>
2022-12-15 00:08:50 -0300

    dockerfile change

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,7 @@ WORKDIR /app
 COPY . .
 
 RUN go build -o server main.go
-RUN go test ./... -coverprofile cover.out
-RUN mkdir testresults   
+RUN go test ./... -coverprofile cover.out   
 RUN go tool cover -html cover.out -o cover.html
 
 EXPOSE 8080
--- STARTpw-check-RESTAPI.sh
+++ STARTpw-check-RESTAPI.sh
@@ -1,10 +1,10 @@
 echo "BE AWARE SUDO IS REQUIRED"
 echo "This code will run docker script"
 echo "a error will happen if don't have docker"
-
+sudo docker rmi $(docker images -q)
 sudo docker build . -t pwcheck-image
 sudo docker run -d -p 8080:8080 --name pwcheck-container pwcheck-image
-echo "Please tell path for tests Results html"
+echo "Please tell new folder for tests Results.html"
 read testpath
 mkdir $testpath
 docker cp pwcheck-container:/app/cover.html $testpath
