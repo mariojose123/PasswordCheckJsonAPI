3872aebdab3a2e77c5f874eff9da04b13e1df2d8
Mariojose123 <mariofilho4k@gmail.com>
2022-12-14 23:38:14 -0300

    dockerfile change

--- STARTpw-check-RESTAPI.sh
+++ STARTpw-check-RESTAPI.sh
@@ -3,7 +3,7 @@ echo "This code will run docker script"
 echo "a error will happen if don't have docker"
 
 sudo docker build . -t pwcheck-image
-sudo docker run  -p 8080:8080 --name pwcheck-container pwcheck-image
+sudo docker run -d -p 8080:8080 --name pwcheck-container pwcheck-image
 echo "Please tell path for tests Results html"
 read testpath
 mkdir $testpath
