cc824325e95fe67330579f49184aafee29458ceb
Mariojose123 <mariofilho4k@gmail.com>
2022-12-14 22:25:14 -0300

    dockerfile change

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,8 @@ WORKDIR /app
 
 COPY . .
 
-RUN go build -o server main.go
-RUN go test ./... -coverprofile cover.out
-RUN mkdir $HOME/testresults   
-RUN go tool cover -html cover.out -o $HOME/cover.html
+RUN go build -o server main.go 
+
 EXPOSE 8080
 
 
--- STARTpw-check-RESTAPI.sh
+++ STARTpw-check-RESTAPI.sh
@@ -4,9 +4,7 @@ echo "a error will happen if don't have docker"
 
 sudo docker build . -t pwcheck-image
 sudo docker run -d -p 8080:8080 --name pwcheck-container pwcheck-imag
-echo "Please tell path for tests Results html"
-read testpath
-mkdir $testpath
-docker cp pwcheck-container:/testresults/ $testpath
+go test ./... -coverprofile cover.out
+go tool cover -html cover.out -o $HOME/cover.html
 
 
