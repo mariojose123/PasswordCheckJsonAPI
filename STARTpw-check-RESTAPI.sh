fcce999b57b4e794cd485dcef19894e2e2c13228
Mariojose123 <mariofilho4k@gmail.com>
2022-12-15 00:11:00 -0300

    dockerfile change

--- STARTpw-check-RESTAPI.sh
+++ STARTpw-check-RESTAPI.sh
@@ -1,7 +1,7 @@
 echo "BE AWARE SUDO IS REQUIRED"
 echo "This code will run docker script"
 echo "a error will happen if don't have docker"
-sudo docker rmi $(docker images -q)
+sudo docker rmi pwcheck-image
 sudo docker build . -t pwcheck-image
 sudo docker run -d -p 8080:8080 --name pwcheck-container pwcheck-image
 echo "Please tell new folder for tests Results.html"
